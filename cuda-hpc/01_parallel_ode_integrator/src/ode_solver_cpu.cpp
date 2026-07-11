0a6d389ab57677c4b03d04b84f5996a06248e89d
Mohamed Elnageeb <mnageeb153@gmail.com>
2026-07-11 03:43:58 +0300

    implemented integrate cpu efficiently

--- cuda-hpc/01_parallel_ode_integrator/src/ode_solver_cpu.cpp
+++ cuda-hpc/01_parallel_ode_integrator/src/ode_solver_cpu.cpp
@@ -10,11 +10,16 @@ void integrate_cpu(OscillatorState* states, const OscillatorParams* params,
     // must match.
     for (int i = 0; i < n; i++)
     {
+        OscillatorState& s = states[i];
+        const OscillatorParams& p = params[i];
+        const float two_zeta_omega = 2.0f * p.zeta * p.omega;
+        const float omega_sq =  p.omega * p.omega;
+        
         for (int j = 0; j < num_steps; j++)
         {
-            float a = -2*params[i].zeta*params[i].omega*states[i].v - params[i].omega*params[i].omega*states[i].x;
-            states[i].v += a*dt ;
-            states[i].x += states[i].v*dt;
+            float a = -two_zeta_omega * s.v - omega_sq * states[i].x;
+            s.v += a * dt;
+            s.x += s.v * dt;
         }
         
     }
